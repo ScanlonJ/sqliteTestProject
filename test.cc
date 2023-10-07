fa3efdd8409eb26be2cef37946039ff4fdacd127
Jacob Scanlon <jacob.scanlon.js@gmail.com>
2023-10-07 17:51:53 -0300

    Initial commit

---
+++ test.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sqlite3.h>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+
+	sqlite3 *db;
+	char *xErrMsg = 0;
+	int rc;
+
+	rc = sqlite3_open( "testDatabase.db", &db );
+
+	if( rc )
+	{
+		std::cout << "Can't open database: %s" << std::endl;
+	}
+	else
+	{
+		std::cout << "Opened database successfully" << std::endl;
+	}
+
+	sqlite3_close( db );
+
+	return 0;
+}
